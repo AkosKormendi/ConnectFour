39e9a8e4a16d0323eca72ce4454a232411c2485a
Körmendi Ákos <akimoki00@gmail.com>
2023-05-27 11:57:53 +0200

    Created responsive menu layout, refactored a bit of js code

--- javascript/table.js
+++ javascript/table.js
@@ -1,17 +1,17 @@
 function GenerateTable()
 {
-    for(let $i = 0; $i < 6; $i++)
+    for(let i = 0; i < 6; i++)
     {
-        let $tr = document.createElement("tr");
-        for(let $j = 0; $j < 7; $j++)
+        let tr = document.createElement("tr");
+        for(let j = 0; j < 7; j++)
         {
-            let $td = document.createElement("td");
-            let $img = document.createElement("img");
-            $img.src = "images/ConnectFourCell.svg";
-            $td.appendChild($img);
-            $tr.appendChild($td);
+            let td = document.createElement("td");
+            let img = document.createElement("img");
+            img.src = "images/ConnectFourCell.svg";
+            td.appendChild(img);
+            tr.appendChild(td);
         }
-        gameTable.appendChild($tr);
+        gameTable.appendChild(tr);
     }
 }
 
